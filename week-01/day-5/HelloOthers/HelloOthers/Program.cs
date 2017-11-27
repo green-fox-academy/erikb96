b7b4fb4835dab2adedb2d38b0b09ca780b8d0ef1
Böködi Erik <b.erik9635@gmail.com>
2017-11-27 15:52:11 +0100

    feat(Visual Studio)Greet Classmates

---
+++ week-01/day-5/HelloOthers/HelloOthers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Misi");
+            Console.WriteLine("Hello András");
+            Console.WriteLine("Hello Marci");
+            Console.ReadLine();
+        }
+    }
+}

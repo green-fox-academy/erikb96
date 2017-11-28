337bcd7f94275676939d63d33270c82205838755
Böködi Erik <b.erik9635@gmail.com>
2017-11-28 08:27:42 +0100

    new folder

---
+++ week-02/day-5/define-basic-info/define-basic-info/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Böködi Erik");
+            name = Console.ReadLine();
+
+            int CalculateAge(DateTime birthDay)
+            {
+                int years = DateTime.Now.Year - birthDay.Year;
+
+                if ((birthDay.Month > DateTime.Now.Month) || (birthDay.Month == DateTime.Now.Month && birthDay.Day > DateTime.Now.Day))
+                    years--;
+
+                return years;
+
+                // Define several things as a variable then print their values
+                // Your name as a string
+                // Your age as an integer
+                // Your height in meters as a double
+                // Whether you are married or not as a boolean
+            }
+    }
+}

d9c849343ba8b5e48d64b02a6664dd1a6d453199
Böködi Erik <b.erik9635@gmail.com>
2017-11-28 14:00:07 +0100

    Done AppendA

---
+++ week-02/day-6/AppendA/AppendA/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string am = "kuty";
+            Console.WriteLine(AppendA(am));
+            Console.ReadKey();
+        }
+        static string AppendA(string text)
+        {
+            return text + "a";
+        }
+    }
+}

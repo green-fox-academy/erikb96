273c7fb634e42b92955c88d2bad0b1aa7bbe4780
Böködi Erik <b.erik9635@gmail.com>
2017-11-29 13:45:20 +0100

    feat(C#)practicing

--- week-02/day-5/define-basic-info/define-basic-info/Program.cs
+++ week-02/day-5/define-basic-info/define-basic-info/Program.cs
@@ -6,24 +6,19 @@ namespace GreenFox
     {
         static void Main(string[] args)
         {
-            string name;
-            Console.WriteLine("Böködi Erik");
-            name = Console.ReadLine();
+            string name = "Böködi Erik";
+            int age = 21;
+            double height = 1.80;
+            bool married = false;
 
-            int CalculateAge(DateTime birthDay)
-            {
-                int years = DateTime.Now.Year - birthDay.Year;
+            Console.WriteLine(name + " | " + age + " | " + height + " | " + married);
+            Console.ReadKey();
 
-                if ((birthDay.Month > DateTime.Now.Month) || (birthDay.Month == DateTime.Now.Month && birthDay.Day > DateTime.Now.Day))
-                    years--;
-
-                return years;
-
-                // Define several things as a variable then print their values
-                // Your name as a string
-                // Your age as an integer
-                // Your height in meters as a double
-                // Whether you are married or not as a boolean
-            }
+            // Define several things as a variable then print their values
+            // Your name as a string
+            // Your age as an integer
+            // Your height in meters as a double
+            // Whether you are married or not as a boolean
+        }
     }
 }
---
+++ week-02/day-6/Summing/Summing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        start:
+
+
+            Console.WriteLine("Value of Numbers");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Sum(number);
+            Console.ReadKey();
+        }
+        static void Sum(int num)
+
+        {
+            int a = num * (num + 1) / 2;
+            Console.WriteLine(a);
+            goto start;
+
+        }
+
+
+        // - Write a function called `sum` that sum all the numbers
+        //   until the given parameter
+
+    }
+
+}

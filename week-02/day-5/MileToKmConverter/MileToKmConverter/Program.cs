f7f9cb4238993427a6f95045b004427b5a44a417
Böködi Erik <b.erik9635@gmail.com>
2017-11-29 16:58:58 +0100

    feat(C#)MilesToKmConverter

---
+++ week-02/day-5/MileToKmConverter/MileToKmConverter/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the kilometers you want to convert :");
+            string distance = Console.ReadLine();
+            int km = int.Parse(distance);
+
+            Console.WriteLine("The distance in miles:" + km * 1.609344);
+            Console.ReadLine();
+
+
+
+
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+        }
+    }
+}

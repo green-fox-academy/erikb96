49e4a07da8a8bcfc583c5c759610ceb06eeef44f
Böködi Erik <b.erik9635@gmail.com>
2017-11-29 15:26:10 +0100

    feat(C#)SecondsInADay

---
+++ week-02/day-5/SecondsInADay/SecondsInADay/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentHours = 15;
+            int currentMinutes = 11;
+            int currentSeconds = 42;
+
+            int twentyfourhour = (24 * 60) * 60;
+            int currenttime = (currentHours * 60)* 60 + currentMinutes * 60 + 42;
+            int targetTime = twentyfourhour - currenttime;
+
+            Console.WriteLine(targetTime + "    seconds remaining till tomorrow");
+            Console.WriteLine(twentyfourhour + "    seconds consists a day");
+            Console.WriteLine(currenttime + "   seconds passed till now");
+               
+            Console.ReadKey();
+
+        }
+    }
+}

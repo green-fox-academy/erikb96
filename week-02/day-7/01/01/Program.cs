5023212386991a048efd7b9e857fc4ede33b82cb
Böködi Erik <b.erik9635@gmail.com>
2017-11-29 12:22:38 +0100

    feat(C#)finish SimpleReplace

---
+++ week-02/day-7/01/01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SimpleReplace
+{
+    public class SimpleReplace
+    {
+        public static void Main(string[] args)
+        {
+            string wrongexample = "In a dishwasher far far away";
+
+            string correctexample = wrongexample.Replace("dishwasher", "galaxy");
+
+          
+            // I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+            // Please fix it for me!
+            // Expected ouput: In a galaxy far far away
+
+            Console.WriteLine(correctexample);
+            Console.ReadKey();
+        }
+    }
+}

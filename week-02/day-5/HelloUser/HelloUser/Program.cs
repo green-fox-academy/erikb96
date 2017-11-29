3216682a73787b9e5bb9d4fbb4b4ba2b0f4ccbc3
Böködi Erik <b.erik9635@gmail.com>
2017-11-29 15:45:13 +0100

    feat(C#)HelloUser

---
+++ week-02/day-5/HelloUser/HelloUser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+           
+       
+            Console.WriteLine("Hello, " + name + "!");
+            Console.ReadLine();
+
+            
+        }
+    }
+}

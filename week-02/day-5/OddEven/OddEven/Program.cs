f93ac8cc344aff518093911f13f8e60831b2cb24
Böködi Erik <b.erik9635@gmail.com>
2017-11-30 11:17:20 +0100

    feat(C#)OddEven

---
+++ week-02/day-5/OddEven/OddEven/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            {
+                if (num % 2 == 0)
+                    Console.WriteLine(num + " Is an Even Number");
+
+
+                else
+                    Console.WriteLine(num + " Is an Odd number");
+            }
+
+            
+            Console.ReadLine();
+                
+
+            // Write a program that reads a number from the standard input,
+            // Then prints "Odd" if the number is odd, or "Even" if it is even.
+
+        }
+    }
+}

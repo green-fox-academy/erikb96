98a9111cc78c1fa0503f02ed0a24c30d8d19c53a
Böködi Erik <b.erik9635@gmail.com>
2017-11-30 14:11:59 +0100

    feat(C#)PrintEven

---
+++ week-02/day-5/PrintEven/PrintEven/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                if (i % 2 == 0) 
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+
+            // Create a program that prints all the even numbers between 0 and 500
+        }
+    }
+}

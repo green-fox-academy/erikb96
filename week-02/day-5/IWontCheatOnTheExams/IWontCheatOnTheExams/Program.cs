fd0068c2843e30e3dd2eef8b976516f2197a73c8
Böködi Erik <b.erik9635@gmail.com>
2017-11-30 14:04:46 +0100

    feat(C#)IWontCheatOnTheExams

---
+++ week-02/day-5/IWontCheatOnTheExams/IWontCheatOnTheExams/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cheat = "I won't cheat on the exam!";
+            for (int i = 1; i < 100; i++)
+            {
+                Console.WriteLine(cheat);
+
+            }
+            Console.ReadLine();
+            // Create a program that writes this line 100 times:
+            // "I won't cheat on the exam!"
+        }
+    }
+}

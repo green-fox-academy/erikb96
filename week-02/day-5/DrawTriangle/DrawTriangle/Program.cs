e4d52b53fa1891eb24576e3afe34dc327be622bc
Böködi Erik <b.erik9635@gmail.com>
2017-11-30 17:35:05 +0100

    feat(C#)DrawTriangle

---
+++ week-02/day-5/DrawTriangle/DrawTriangle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many lines should the triangle have?" );
+            int lines = Convert.ToInt32(Console.ReadLine());
+            string star = "*";
+            for (int i = 0; i < lines; i++)
+            {
+            Console.WriteLine(star);
+                star += "*";
+            }
+            Console.ReadKey();
+
+
+            
+            
+
+            // Write a program that reads a number from the standard input, then draws a
+            // triangle like this:
+            //
+            // *
+            // **
+            // ***
+            // ****
+            //
+            // The triangle should have as many lines as the number was
+        }
+
+       
+    }
+}
+

9d0ccb34255d32ee1f8ee02de1b05a4577806e4b
Böködi Erik <b.erik9635@gmail.com>
2017-11-30 19:47:55 +0100

    feat(C#)DrawPyramid

---
+++ week-02/day-5/DrawPyramid/DrawPyramid/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many lines should the pyramid have?");
+            int lines = Convert.ToInt32(Console.ReadLine());
+
+
+            string star = "*";
+
+            for (int i = 0; i < lines; i++)
+            {
+                for (int ii = i; ii < lines - 1; ii++)
+                {
+                    Console.Write(" ");
+                }
+
+                {
+                    Console.WriteLine(star);
+                    star += "**";
+                }
+
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
+        

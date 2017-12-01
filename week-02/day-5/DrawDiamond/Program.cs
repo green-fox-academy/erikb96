d4d339904f693031e1fb0c5766ae1afee0bec3f2
Böködi Erik <b.erik9635@gmail.com>
2017-12-01 11:58:34 +0100

    feat(C#)GuessTheNumber

---
+++ week-02/day-5/DrawDiamond/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many lines should the diamond have?");
+            int lines = Convert.ToInt32(Console.ReadLine());
+            string stars = "*";
+            for (int i = 0; i < lines; i++)
+                for (int e = i; e < lines; e++)
+                {
+                    Console.WriteLine(" ");
+                }
+            {
+                Console.WriteLine(stars);
+                stars += "**";
+            }
+            //for (int i = 0; i < lines; i--)
+                for (int e = i; e < lines; e--)
+                {
+                    Console.WriteLine(" ");
+                }
+            {
+                Console.WriteLine(stars);
+                stars += "**";
+               
+                
+            }
+          
+            Console.ReadLine();
+            
+            
+            // Write a program that reads a number from the standard input, then draws a
+            // diamond like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //  *****
+            //   ***
+            //    *
+            //
+            // The diamond should have as many lines as the number was
+
+        }
+    }
+}

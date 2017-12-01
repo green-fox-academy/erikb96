9e58c4c36a24dbebe8f3b3dceba6e05ce50bf946
Böködi Erik <b.erik9635@gmail.com>
2017-12-01 11:47:58 +0100

    feat(C#)GuessTheNumber

---
+++ week-02/day-9/GuessTheNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd =  new Random();
+            int thoughtnumber = rnd.Next(1, 20);
+
+            again:
+            
+            Console.WriteLine("Which number do i think?");
+            int yournumber = Convert.ToInt32(Console.ReadLine());
+
+            if (yournumber == thoughtnumber)
+            {
+                Console.WriteLine("You found the number: " + thoughtnumber);
+            }
+            else if (yournumber > thoughtnumber)
+            {
+                Console.WriteLine("The stored number is lower");
+                goto again;
+            }
+            else
+            {
+                Console.WriteLine("The stored number is higher");
+                goto again;
+            }
+            Console.ReadLine();
+          
+        }
+    }
+}

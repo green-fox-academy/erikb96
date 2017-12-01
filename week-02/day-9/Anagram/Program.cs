b1e93f5ea5ee30281b33a567bcb4c73708dcaa3f
Böködi Erik <b.erik9635@gmail.com>
2017-12-01 10:56:48 +0100

    feat(C#)Anagram

---
+++ week-02/day-9/Anagram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.Write("Enter first word:");
+            string word1 = Console.ReadLine();
+            Console.Write("Enter second word:");
+            string word2 = Console.ReadLine();
+                                         
+            char[] char1 = word1.ToLower().ToCharArray();
+            char[] char2 = word2.ToLower().ToCharArray();
+                         
+            Array.Sort(char1);
+            Array.Sort(char2);
+                         
+            string NewWord1 = new string(char1);
+            string NewWord2 = new string(char2);
+                      
+            if (NewWord1 == NewWord2)
+            {
+                Console.WriteLine("True! Words \"{0}\" and \"{1}\" are Anagrams", word1, word2);
+            }
+            else
+            {
+                Console.WriteLine("False! Words \"{0}\" and \"{1}\" are not Anagrams", word1, word2);
+            }
+                Console.ReadLine();
+        }
+    }
+}

6969747c3c602e2337afd811dc132bb849c8927b
Böködi Erik <b.erik9635@gmail.com>
2017-12-03 13:22:03 +0100

    feat(C#)SwapElements

---
+++ week-02/day-6/AppendA2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nimals = { "kuty" ,  "macsk" , "cic" };
+            for (int i = 0; i < nimals.Length; i++)
+            {
+                nimals[i] = nimals[i] + "a";
+                Console.WriteLine(nimals[i]);
+            }
+            // - Create an array variable named `nimals`
+            //   with the following content: `["kuty", "macsk", "cic"]`
+            // - Add all elements an `"a"` at the end
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-02/day-6/SwapElements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] abc = { "first", "second", "third" };
+            for (int i = 0; i < abc.Length; i++)
+            {
+                string swap = abc[0];
+                abc[0] = abc[2];
+                abc[2] = swap;
+                Console.WriteLine(abc[i]);
+
+            }
+          
+            
+            // - Create an array variable named `abc`
+            //   with the following content: `["first", "second", "third"]`
+            // - Swap the first and the third element of `abc`
+            Console.ReadLine();
+        }
+    }
+}

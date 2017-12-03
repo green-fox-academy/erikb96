b921416f836a38cb5dd68ebf21348d02f97fc0e9
Böködi Erik <b.erik9635@gmail.com>
2017-12-03 11:49:23 +0100

    feat(C#)PrintAll

---
+++ week-02/day-6/PrintAll/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PrintAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] af = { 4, 5, 6, 7 };
+            Console.WriteLine(af);
+            for (int i = 0; i < af.Length; i++)
+            {
+                Console.WriteLine(af[i]);
+            }
+            // - Create an array variable named `af`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print all the elements of `af`
+            Console.ReadLine();
+        }
+    }
+}

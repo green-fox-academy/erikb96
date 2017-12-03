962e8cfc90209c1d6895dcd025a7d22247b37da7
Böködi Erik <b.erik9635@gmail.com>
2017-12-03 21:01:09 +0100

    feat(C#)Reverse

---
+++ week-02/day-7/Reverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse
+{
+    public class Reverse
+    {
+        public static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+            reversed = RecursivelyReverseString(reversed);
+            Console.WriteLine(reversed);
+            Console.ReadLine();
+        }
+
+        public static string RecursivelyReverseString(string s)
+        {
+            if (s.Length > 0)
+                return s[s.Length - 1] + RecursivelyReverseString(s.Substring(0, s.Length - 1));
+            else
+                return s;
+          }
+    }
+}

9108d62dd8c9754d2eb6f752a5fb4b2dc716f293
Böködi Erik <b.erik9635@gmail.com>
2017-12-03 14:07:05 +0100

    feat(C#)Reverse

---
+++ week-02/day-6/Reverse/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] aj = { 3, 4, 5, 6, 7 };
+            Array.Reverse(aj);
+            for (int i = 0; i < aj.Length; i++)
+            {
+                
+                Console.WriteLine(aj[i]);
+
+            }
+           
+            // - Create an array variable named `aj`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Reverse the order of the elements in `aj`
+            // - Print the elements of the reversed `aj`
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-02/day-6/SumAllElements/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ai = { 3, 4, 5, 6, 7 };
+            int sum = ai[0] + ai[1] + ai[2] + ai[3] + ai[4];
+            Console.WriteLine(sum);
+            
+           
+            // - Create an array variable named `ai`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Print the sum of the elements in `ai`
+            Console.ReadLine();
+        }
+    }
+}

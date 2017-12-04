5a3d67aad05166f1a7b9493215241d8d54e36aea
Böködi Erik <b.erik9635@gmail.com>
2017-12-04 14:52:57 +0100

    feat(C#)WriteSingleLine

---
+++ week-03/day-1/WriteSingleLine/WriteSingleLine/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace WriteSingleLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"my-file.txt";
+
+            try
+            {
+                if(path == @"my-file.txt")
+                {
+                    StreamWriter file = new StreamWriter(path);
+                file.WriteLine("Böködi Erik");
+                    file.Close();
+
+                }
+                else
+                {
+                    throw new Exception();
+                }
+                
+                
+                
+             
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to write file: my-file.txt");
+
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
+// Open a file called "my-file.txt"
+// Write your name in it as a single line
+// If the program is unable to write the file,
+// then it should print an error message like: "Unable to write file: my-file.txt"

ba7dc2150208c9463e206922c902fb58eeac0d4c
Böködi Erik <b.erik9635@gmail.com>
2017-12-06 15:25:14 +0100

    feat(C# WPF)PositionSquare

---
+++ week-03/day-3/PositionSquare/PositionSquare/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace PositionSquare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            SquareDrawer(foxDraw);
+        }
+        public static void SquareDrawer(FoxDraw foxDraw)
+        {
+            int x = 0;
+            int y = 0;
+            for (int i = 1; i < 3; i++)
+            {
+                foxDraw.DrawRectangle(x, y, 50, 50);
+                x = x +  50;
+                y = y +  50;
+
+
+            }
+        }
+    }
+}
+// create a square drawing function that takes 2 parameters:
+// the x and y coordinates of the square's top left corner
+// and draws a 50x50 square from that point.
+// draw 3 squares with that function.            

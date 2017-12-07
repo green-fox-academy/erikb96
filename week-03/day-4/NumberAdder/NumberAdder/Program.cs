49e5db4ef6dd1b564ae12e7ff6de2d0d99b3b121
Böködi Erik <b.erik9635@gmail.com>
2017-12-07 13:42:08 +0100

    feat(C#)NumberAdder

---
+++ week-03/day-4/NumberAdder/NumberAdder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numberadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add a number!");
+            int mynumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("The sum of the numbers 1 - " + mynumber + ": " + NumberAdder(mynumber));
+            Console.ReadLine();
+        }
+        public static int NumberAdder(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else 
+            {
+                return number + NumberAdder(number - 1);
+            }
+
+        }
+    }
+}

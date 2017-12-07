24ef4dc21d6176e6d8d226e03cbe74a08d77908b
Böködi Erik <b.erik9635@gmail.com>
2017-12-07 11:51:16 +0100

    feat(C#)Counter

---
+++ week-03/day-4/Counter/Counter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mynumber = 1000;
+            Counter(mynumber);
+            Console.ReadLine();
+            
+        }
+        public static int Counter(int number)
+        {
+            Console.WriteLine(number);
+            if (number == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return Counter(number - 1);
+            }
+            
+            
+        }
+    }
+}
+// Write a recursive function that takes one parameter: n and counts down from n.

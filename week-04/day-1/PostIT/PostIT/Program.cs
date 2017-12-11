5f186e5221b3582fee415b3746e1e2e817960a75
Böködi Erik <b.erik9635@gmail.com>
2017-12-11 14:04:31 +0100

    feat(C#)PostIt

---
+++ week-04/day-1/PostIT/PostIT/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PostIT
+{
+    class Program
+    {
+        struct PostIt
+        {
+            public string BackgroundColor;
+            public string Text;
+            public string TextColor;
+        }
+        static void Main(string[] args)
+        {
+            PostIt postIT1;
+            postIT1.BackgroundColor = "orange";
+            postIT1.Text = "Idea1";
+            postIT1.TextColor = "blue";
+
+            PostIt postIT2;
+            postIT2.BackgroundColor = "pink";
+            postIT2.Text = "Awesome";
+            postIT2.TextColor = "black";
+
+            PostIt postIT3;
+            postIT3.BackgroundColor = "yellow";
+            postIT3.Text = "Superb";
+            postIT3.TextColor = "green";
+
+            Console.WriteLine(postIT2.Text);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}

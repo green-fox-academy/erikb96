31cf3a6da3448c4b2d0372bc1dc2bb8d8c38d5b3
Böködi Erik <b.erik9635@gmail.com>
2017-12-20 15:58:51 +0100

    feat(C#) Doubling

---
+++ Practice/Doubling/Doubling/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Doubling
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Practice/Doubling1/Doubling1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ak = Doubling(123);
+            Console.WriteLine(ak);            
+            Console.ReadLine();
+        }
+
+
+        public static int Doubling(int ak)
+        {
+            return ak *= 2;
+        }
+        // - Create an integer variable named `ak` and assign the value `123` to it
+        // - Create a function called `doubling` that doubles it's input parameter
+        // - Print the result of `doubling(ak)`
+    }
+}
